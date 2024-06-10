f61c3ca4311835ca4b2035a6211c60b404233b9d
Daniil Murzin <danmur92@mail.ru>
2024-06-10 21:23:09 +0500

    add main UI + Navigation

---
+++ EPIC_RPS/View/SettingsView.swift
@@ -0,0 +1,25 @@
+//
+//  SettingsView.swift
+//  EPIC_RPS
+//
+//  Created by Daniil Murzin on 10.06.2024.
+//
+
+
+import SwiftUI
+
+struct SettingsView: View {
+    //MARK: - PROPERTIES
+    
+    //MARK: - BODY
+    var body: some View {
+        Text("Экран SettingsViewController")
+    }
+    //MARK: - FUNCTIONS
+    
+}
+
+//MARK: - PREVIEW
+#Preview {
+    FightLoadView()
+}
--- EPIC_RPS/View/SplashView.swift
+++ EPIC_RPS/View/SplashView.swift
@@ -12,10 +12,47 @@ struct SplashView: View {
     
     //MARK: - BODY
     var body: some View {
-        VStack {
-            Text("Главный экран")
-        }//: VSTACK
-        .padding()
+        NavigationView {
+            VStack {
+                
+                HStack {
+                    NavigationLink(destination: SettingsView()){
+                        Image(.settings)
+                            .resizable()
+                            .frame(width: 35, height: 35)
+                            .foregroundStyle(.red)
+                    }
+                    .padding(.leading, 20)
+                    
+                    Spacer()
+                    
+                    NavigationLink(destination: RulesView()){
+                        Image(.rules)
+                            .resizable()
+                            .frame(width: 35, height: 35)
+                    }
+                    .padding(.trailing, 20)
+                }
+                
+                
+                Spacer().frame(height: 80)
+                
+                Image(.epicRPSLogo)
+                
+                Spacer().frame(height: 30)
+                
+                VStack(spacing: 15) {
+                    GameControlButtons(buttonText: "Start game", destination: StartGameView())
+                    GameControlButtons(buttonText: "Results",destination: FightResultView())
+                }
+            }
+        }
+        
+        .background(LinearGradient(
+            gradient: Gradient(colors: [.splashBG1, .splashBG2]),
+            startPoint: .top,
+            endPoint: .bottom
+        ))
     }
     //MARK: - FUNCTIONS
     
