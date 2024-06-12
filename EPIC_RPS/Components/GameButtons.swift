396604858f906c063c7354116c055cc4c0dd439a
Dmitry <dimasel@gmail.com>
2024-06-12 11:46:32 +0700

    New control panel

---
+++ EPIC_RPS/Components/GameButtons.swift
@@ -0,0 +1,18 @@
+//
+//  GameButtons.swift
+//  EPIC_RPS
+//
+//  Created by Dmitriy Eliseev on 11.06.2024.
+//
+
+import SwiftUI
+
+struct GameButtons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameButtons()
+}
